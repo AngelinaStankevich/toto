96dd0454d9b68aaf8ef13796672d43efa11cef7c
StankevichAngelina <79199054+StankevichAngelina@users.noreply.github.com>
2021-04-29 20:23:50 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,38 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace Ugaday_chislo
+namespace GDIPainter
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-
-
-            char again = 'y';
-            Random rand = new Random();
-
-            while (again == 'y')
-            {
-                int i = rand.Next(50);
-
-                Console.WriteLine("Компьютер загадал число от 0 до 50");
-
-                if (i < 25) Console.WriteLine("Число меньше 25");
-                else Console.WriteLine("Число больше или равно 25");
-
-                int x = Convert.ToInt32(Console.ReadLine());
-
-                if (i == x) Console.WriteLine("Поздравляем! Вы победили свой компьютер!");
-                else Console.WriteLine("Вы проиграли! Компьютер загадал число {0}", i);
-
-                Console.WriteLine("Попробовать еще? (y = Да, n = Нет)");
-                again = Convert.ToChar(Console.ReadLine());
-
-            }
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMain());
         }
     }
 }
