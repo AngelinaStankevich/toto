e42725c5cd2a88049613881c8c77baf84da34392
StankevichAngelina <79199054+StankevichAngelina@users.noreply.github.com>
2021-06-04 21:08:08 +0300

    Add files via upload

---
+++ Lab6/ConsoleApp1/BodyData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class BodyData:IComparable<BodyData>
+    {
+        public string Name { get; }
+        public float Weight { get; }
+        public float Height { get; }
+        public int NormalAmountOfFood { get; }
+        BodyData() { }
+        public BodyData(string name, float weight, float height, int normalAmountOfFood) 
+        {
+            Name = name;
+            Weight = weight;
+            Height = height;
+            NormalAmountOfFood = normalAmountOfFood;
+        }
+        public int CompareTo(BodyData obj) 
+        {
+            if (Weight > obj.Weight) return 1;
+            if (Weight < obj.Weight) return -1;
+            else return 0;
+        }
+        public override string ToString()
+        {
+            return String.Format("Name: {0}\tWeight: {1}\t Height: {2}\t Food allowance: {3}\t", Name, Weight, Height, NormalAmountOfFood);
+        }
+    }
+}
